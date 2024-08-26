1374b46d75c8bf199c2672cf599a9227ef5b6320
Svetlana Molodtsova <167689948+SMolodtsova13@users.noreply.github.com>
2024-08-26 16:59:24 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.172.126', 'localhost']
-                 # '<your_domain>']
+ALLOWED_HOSTS = ['89.169.172.126', 'localhost', 'molodtsova-taski.webhop.me']
 
 # Application definition
 
